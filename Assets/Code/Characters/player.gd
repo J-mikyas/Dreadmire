468b12d5f464e10e58e20314f547fdaa5155c502
J-mikyas <miki53852@gmail.com>
2026-09-19 20:52:24 +0300

    Feat: Add hand-mouse aligned rotations.

--- Assets/Code/Characters/player.gd
+++ Assets/Code/Characters/player.gd
@@ -4,8 +4,24 @@ var direction: Vector2 = Vector2.ZERO
 const SPEED = 600
 const FRICTION = 3000
 
+@onready var hand: Node2D = $Hand
+@onready var hand_sprite_2d: Sprite2D = $Hand/HandSprite2D
 
 func _physics_process(delta: float) -> void:
+	
+	# \\ GUN AND HAND SYSTEM //
+	
+	var mouse_pos = get_global_mouse_position() - global_position
+	var mouse_angle = rad_to_deg(mouse_pos.angle())
+	
+	hand.rotation_degrees = mouse_angle - 180
+	
+	if not (mouse_angle <= 90 and mouse_angle >= -90):
+		hand_sprite_2d.flip_v = true
+	else:
+		hand_sprite_2d.flip_v = false
+	
+	# \\ MOVEMENT SYSTEM //
 	
 	direction = Input.get_vector("left","right","up","down")
 	var target_pos = direction * SPEED
