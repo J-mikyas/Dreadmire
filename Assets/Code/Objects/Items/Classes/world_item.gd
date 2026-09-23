09db06b358567ce312ecfd369509614e780edb92
J-mikyas <miki53852@gmail.com>
2026-09-23 23:23:54 +0300

    Fix: Fix item merge conflict.

--- Assets/Code/Objects/Items/Classes/world_item.gd
+++ Assets/Code/Objects/Items/Classes/world_item.gd
@@ -1,24 +1,27 @@
 class_name World_Item
 extends Item
 
-@onready var interact_key: Label = $Interact_Key
-@onready var amount_text: Label = $Amount
+@onready var interact_key: Label = $Visible/Interact_Key
+@onready var amount_text: Label = $Visible/Amount
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var amount:int = 1
 
 var can_PickUp: bool = false 
+
+func _ready() -> void:
+	update_amount()
 
 func PickUp():
 	Inventory.append_item({Item_Name:amount})
 	queue_free()
 
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_proximity_prompt_radius_body_entered(body: Node2D) -> void:
 	if body is Player:
 		interact_key.text = "[E]"
 		interact_key.show()
 		can_PickUp = true
 
-func _on_body_exited(body: Node2D) -> void:
+func _on_proximity_prompt_radius_body_exited(body: Node2D) -> void:
 	if body is Player:
 		interact_key.hide()
 		can_PickUp = false
@@ -45,8 +48,8 @@ func merge(other_item: World_Item) -> void:
 	update_amount()
 
 func _on_merge_radius_area_entered(area: Area2D) -> void:
-	var other_item = area.get_parent()
+	var other_item = area.get_parent().get_parent()
 
-	if (other_item is World_Item) and (Item_Name == other_item.Item_Name):
+	if (other_item is World_Item) and (Item_Name == other_item.Item_Name) and not other_item.is_queued_for_deletion():
 		if get_instance_id() > other_item.get_instance_id():
 			merge(other_item)
