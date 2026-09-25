25de25fb0e4e1189b8a29bb759036278890f945a
J-mikyas <miki53852@gmail.com>
2026-09-25 19:10:12 +0300

    Feat: Add item merge animations.

--- Assets/Code/Objects/Items/Classes/world_item.gd
+++ Assets/Code/Objects/Items/Classes/world_item.gd
@@ -33,6 +33,9 @@ func _input(event: InputEvent) -> void:
 
 # \\ Merge Logic //
 
+var is_merging:bool = false
+
+
 func update_amount() -> void:
 	if amount > 1:
 		amount_text.show()
@@ -40,15 +43,52 @@ func update_amount() -> void:
 	else:
 		amount_text.hide()
 
+func play_merge_anim() -> void:
+	var tween: Tween = create_tween()
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	
+	tween.tween_property(self, "scale", Vector2(0.5, 0.5), 0.15)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.15)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
+
 func merge(other_item: World_Item) -> void:
 	
-	amount += other_item.amount
-	other_item.queue_free()
-	update_amount()
+	is_merging = true
+	other_item.is_merging = true
+	
+	$Visible/MergeRadius.set_deferred("monitoring", false)
+	$Visible/MergeRadius.set_deferred("monitorable", false)
+	other_item.get_node("Visible/MergeRadius").set_deferred("monitoring", false)
+	other_item.get_node("Visible/MergeRadius").set_deferred("monitorable", false)
+	
+	var tween: Tween = create_tween()
+	tween.tween_property(other_item,"position",self.position,0.2)
+	tween.set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+	await tween.finished
+	
+	if is_instance_valid(other_item):
+		amount += other_item.amount
+		other_item.queue_free()
+		update_amount()
+		play_merge_anim()
+	
+	is_merging = false
+	
+	$Visible/MergeRadius.monitoring = true
+	$Visible/MergeRadius.monitorable = true
+	other_item.get_node("Visible/MergeRadius").set_deferred("monitoring", true)
+	other_item.get_node("Visible/MergeRadius").set_deferred("monitorable", true)
+	
+	await get_tree().physics_frame
+		
+	var overlapping_areas = $Visible/MergeRadius.get_overlapping_areas()
+	for area in overlapping_areas:
+		_on_merge_radius_area_entered(area)
+
 
 func _on_merge_radius_area_entered(area: Area2D) -> void:
 	var other_item = area.get_parent().get_parent()
 
-	if (other_item is World_Item) and (self.item_name == other_item.item_name) and not other_item.is_queued_for_deletion():
+	if (other_item is World_Item) and not (is_merging or other_item.is_merging) and (self.item_name == other_item.item_name) and not other_item.is_queued_for_deletion():
 		if get_instance_id() > other_item.get_instance_id():
 			merge(other_item)
