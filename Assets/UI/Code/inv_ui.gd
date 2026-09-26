357f9c3deb257b3ae94285db51cf64dcdb1e5578
J-mikyas <miki53852@gmail.com>
2026-09-26 14:49:31 +0300

    Feat: Add basic backpack UI.

--- Assets/UI/Code/inv_ui.gd
+++ Assets/UI/Code/inv_ui.gd
@@ -3,12 +3,28 @@ extends Control
 # \\ Inventory UI //
 
 @onready var quick_inv: HBoxContainer = $QuickInv
+@onready var backpack_inv: ScrollContainer = $BackpackInv
+@onready var backpack_items: GridContainer = $BackpackInv/BackpackItems
 
 
 var InvBox = preload("res://Assets/UI/Scenes/InvBox.tscn")
+var BackpackBox = preload("res://Assets/UI/Scenes/BackpackBox.tscn")
 
 func _ready() -> void:
 	Inventory.changed.connect(add_box)
+	add_box()
+
+func add_backpack_box():
+	var backpack_box:Button = BackpackBox.instantiate()
+	
+	backpack_box.pressed.connect(func():
+		if backpack_inv.visible:
+			backpack_inv.hide()
+		else:
+			backpack_inv.show()
+	)
+	
+	quick_inv.add_child(backpack_box)
 
 func add_box():
 	
@@ -16,8 +32,10 @@ func add_box():
 	
 	for box in quick_inv.get_children():
 		box.queue_free()
+	for box in backpack_items.get_children():
+		box.queue_free()
 	
-	# populate
+	# populate quick
 	for i in Inventory.inventory["quick"].size():
 		
 		
@@ -38,3 +56,22 @@ func add_box():
 		box._pressed.connect(func():
 			Inventory.toggle_item(item_name)
 			)
+	
+	#add backpack button
+	
+	add_backpack_box()
+	
+	# populate backpack
+	for i in Inventory.inventory["backpack"].size():
+		var box:InvBoxUI = InvBox.instantiate()
+		
+		var item = Inventory.inventory["backpack"][i]
+		var item_name = item.keys()[0]
+		var amount = item.values()[0]
+		var img = Inventory.get_icon(item_name)
+		
+		box.Item_Name = item_name
+		box.Amount = amount
+		box.Img = img
+		
+		backpack_items.add_child(box)
